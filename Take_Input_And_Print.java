a3df9fea32f279975e89887b8eaa8f41bd4751e4
Kbhagyasri <119720683+Kbhagyasri@users.noreply.github.com>
2023-06-17 21:28:50 +0530

    import java.util.*; public class praveen{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         String n;         n=sc.nextLine();         System.out.println(n);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class praveen{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String n;
+        n=sc.nextLine();
+        System.out.println(n);
+    }
+}
